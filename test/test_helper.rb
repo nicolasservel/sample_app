01e0eb1c0cad48c49bdcc963f9d550bd5145918d
nicolasservel <nico.servel@gmail.com>
2014-10-21 18:55:05 +0200

    save middle changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,7 @@ class ActiveSupport::TestCase
   # -- they do not yet inherit this setting
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+  	!session[:user_id].nil?
+  end
 end
