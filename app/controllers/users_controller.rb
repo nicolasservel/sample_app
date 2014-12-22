dba0346d0a891c3c720d3ce007f0d10bd3882858
nicolasservel <nico.servel@gmail.com>
2014-12-22 15:52:41 +0100

    adding the database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,41 @@
 class UsersController < ApplicationController
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
-  def new 
-  	@user = User.new
+  def new
+    @user = User.new
   end
 
-  def create 
-  	@user = User.new(user_params)
-  	if @user.save
+  def create
+    @user = User.new(user_params)
+    if @user.save
       log_in @user
-      flash[:success]="Welcome to the Sample App!"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def edit
     @user = User.find(params[:id])
-    if @user.udpate_attributes(user_params)
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
     else
       render 'edit'
     end
   end
 
-  private 
+  private
 
     def user_params
-     params.require(:user).permit(:name,:email,:password,
-                                 :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-
-
 end
